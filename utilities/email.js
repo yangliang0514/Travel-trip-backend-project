7410dfab1b4eed25a535ad82fd4f4d6706e6be45
Kevin Yang <e510021@gmail.com>
2023-04-12 02:40:51 +0800

    create email class

--- utilities/email.js
+++ utilities/email.js
@@ -1,22 +1,62 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const { convert } = require("html-to-text");
 
-module.exports = async (options) => {
-  // 1. Create a transporter (mail service)
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2. define the email options
-  const mailOptions = {
-    from: "Kevin Yang <Kevin@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // 3. Sending the email
-  await transporter.sendMail(mailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Kevin Yang <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid email service
+      return;
+    }
+
+    if (process.env.NODE_ENV === "development") {
+      // 1. Create a transporter (mail service)
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+    }
+  }
+
+  async send(template, subject) {
+    // send the actual email
+    // 1. Render the html for the email based on a pug template
+    // turn a pug template into a html
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    // 2. define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      // use html-to-text package to transform html into plain text
+      text: convert(html),
+    };
+
+    // 3. Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Natours family!");
+  }
 };
