15e460954086282bfc1e83b09a535ccd89a8a306
Kevin Yang <e510021@gmail.com>
2023-04-15 00:06:54 +0800

    stripe payment backend

---
+++ Routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bookingController = require("../controllers/bookingController");
+const authController = require("./../controllers/authController");
+
+const router = express.Router();
+
+// this route is only used to getting the checkout-sessions
+router.get(
+  "/checkout-session/:tourId",
+  authController.protect,
+  bookingController.getSession
+);
+
+module.exports = router;
---
+++ controllers/bookingController.js
@@ -0,0 +1,40 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utilities/catchAsync");
+const AppError = require("../utilities/appError");
+const factory = require("./handlerFactory");
+
+exports.getSession = catchAsync(async (req, res, next) => {
+  // 1. get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  // 2. create a checkout session
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    payment_method_types: ["card"],
+    success_url: `${req.protocol}://${req.get("host")}/`,
+    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: "usd",
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
+  });
+
+  // 3. send it to the client
+  res.status(200).json({
+    status: "success",
+    session,
+  });
+});
